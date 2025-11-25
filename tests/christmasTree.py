a2b756fe20f277d1e6246fa82b5e7bbe123bb119
LeganSar44 <hoangslevan@gmail.com>
2025-11-25 17:29:54 +0700

    Add full lyrics for "All I Want for Christmas Is You" and update music file path

--- tests/christmasTree.py
+++ tests/christmasTree.py
@@ -17,13 +17,68 @@ COLORS=[
 
 
 lyrics=[
-    "I don't want a lot for Christmas",
-    "There is just one thing I need",
-    "I don't care about the presents underneath the Christmas tree",
-    "I just want you for my own",
-    "More than you could ever know",
-    "Make my wish come true",
-    "All I want for Christmas is you"
+  "(Intro)",
+  "I don't want a lot for Christmas",
+  "There is just one thing I need",
+  "I don't care about the presents underneath the Christmas tree",
+  "I just want you for my own",
+  "More than you could ever know",
+  "Make my wish come true",
+  "All I want for Christmas is you, yeah.",
+  "",
+  "(Verse 1)",
+  "I don't want a lot for Christmas",
+  "There is just one thing I need, and I",
+  "Don't care about the presents underneath the Christmas tree",
+  "I don't need to hang my stocking there upon the fireplace",
+  "Santa Claus won't make me happy with a toy on Christmas Day",
+  "",
+  "(Chorus)",
+  "I just want you for my own",
+  "More than you could ever know",
+  "Make my wish come true",
+  "All I want for Christmas is you",
+  "You, baby",
+  "",
+  "(Verse 2)",
+  "Oh, I won't ask for much this Christmas",
+  "I won't even wish for snow, and I",
+  "I just wanna keep on waiting underneath the mistletoe",
+  "I won't make a list and send it to the North Pole for Saint Nick",
+  "I won't even stay awake to hear those magic reindeer click",
+  "",
+  "(Chorus)",
+  "'Cause I just want you here tonight",
+  "Holding on to me so tight",
+  "What more can I do?",
+  "Oh, baby, all I want for Christmas is you",
+  "You, baby",
+  "",
+  "(Bridge)",
+  "Oh, all the lights are shining so brightly everywhere",
+  "And the sound of children's laughter fills the air",
+  "And everyone is singing",
+  "I hear those sleigh bells ringing",
+  "Santa, won't you bring me the one I really need?",
+  "Won't you please bring my baby to me?",
+  "",
+  "(Verse 3)",
+  "Oh, I don't want a lot for Christmas",
+  "This is all I'm asking for",
+  "I just wanna see my baby standing right outside my door",
+  "",
+  "(Chorus)",
+  "Oh, I just want you for my own",
+  "More than you could ever know",
+  "Make my wish come true",
+  "Oh, baby, all I want for Christmas is you",
+  "You, baby",
+  "",
+  "(Outro)",
+  "All I want for Christmas is you, baby",
+  "All I want for Christmas is you, baby",
+  "All I want for Christmas is you, baby",
+  "All I want for Christmas is you, baby"
 ]
 
 
@@ -63,10 +118,12 @@ def draw_tree(tick):
 
         # print(" " * padding , end = "")
         #CHRISTMAS TREE
-
-        for _ in range(2 * i + 1):
-            color = random.choice(COLORS)
-            print(color +  "*"  + "\033[0m", end = "")
+        if i ==0:
+            print("\033[33m✯", end = "")
+        else: 
+            for _ in range(2 * i + 1):
+                color = random.choice(COLORS)
+                print(color +  "*"  + "\033[0m", end = "")
         right_padding=(HEIGHT -i) + 5
 
         # SNOW RIGHT (NEW)
@@ -95,8 +152,11 @@ def draw_tree(tick):
 
 def init_music():
     pygame.mixer.init()
+    code_path = os.path.dirname(os.path.abspath(__file__))
+    song_path = os.path.join(code_path,"res","christmas_song.mp3")
+    print(song_path)
     try:
-        pygame.mixer.music.load("christmas_song.mp3")
+        pygame.mixer.music.load(song_path)
         pygame.mixer.music.play(-1) # loop forever
     except:
         print("Chua co file nhac 'christmas_song.mp3!' ")
@@ -112,7 +172,7 @@ def main():
             update_snow()
             draw_tree(tick)
             tick+=1
-            time.sleep(0.1)
+            time.sleep(0.08)
     except KeyboardInterrupt:
         pygame.mixer.music.stop()
         print("\033[34mMerryChrismas 🎁")
