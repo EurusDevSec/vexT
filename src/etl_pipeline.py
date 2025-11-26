31c965f5ce1635bfbb04b930199a6b520e556ac1
LeganSar44 <hoangslevan@gmail.com>
2025-11-26 15:47:18 +0700

    Add initial implementation of ETL pipeline for product data normalization

---
+++ src/etl_pipeline.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np 
+# from sentence_transformer import SentenceTransformer
+import os
+
+#CONFIG
+
+# print("Loading model AI... ")
+# model = SentenceTransformer('all-MiniLM-L6-v2')
+
+
+dir_script = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+file_path= os.path.join(dir_script,"res", "products.csv")
+
+# print(csv_path)
+def normalize_data(file_path):
+    # df = pd.DataFrame(file_path)
+    # print(df.head())
+    
+    df =pd.read_csv(file_path)
+    print(df)
+    df["category"] = df["category"].fillna("Unknown")
+    df["title"] = df["title"].fillna("Unknown")
+    print(df["category"])
+    print(df["title"])
+def main():
+    normalize_data(file_path)
+    
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
